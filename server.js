515a1665ad272ea2e9d82c77157cd06af504da6d
Halid Cisse <77756563+Halid04@users.noreply.github.com>
2024-01-19 01:14:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  next();
+});
+
+
 app.use(bodyParser.json());
 
 app.post("/process", async (req, res) => {
